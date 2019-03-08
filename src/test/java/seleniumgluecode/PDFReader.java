b1d430cb9df3ab02aac637e6add9e87e89a50197
Manohar Shimpi <mashimpi@in.ibm.com>
2019-03-08 22:02:18 +0530

    Removed unused import statement...

--- src/test/java/seleniumgluecode/PDFReader.java
+++ src/test/java/seleniumgluecode/PDFReader.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
